e814917ee123bae1637a75e0743e50d6bc07f930
lulujianglab <18810330250@163.com>
2019-01-14 09:53:01 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class LogWebpackPlugin {
 
   constructor(doneCallback, emitCallback) {
     this.emitCallback = emitCallback
-    this.doneCallback = doneCallback   
+    this.doneCallback = doneCallback
   }
 
   apply(compiler) {
@@ -13,10 +13,20 @@ class LogWebpackPlugin {
     //   this.doneCallback()
     // })
     compiler.hooks.emit.tap('LogWebpackPlugin', () => {
+      // 在 emit 事件中回调 emitCallback
       this.emitCallback()
     })
     compiler.hooks.done.tap('LogWebpackPlugin', () => {
+      // 在 done 事件中回调 doneCallback
       this.doneCallback()
+    })
+    compiler.hooks.compilation.tap('LogWebpackPlugin', () => {
+      // compilation（'编译器'对'编译ing'这个事件的监听）
+      console.log("The compiler is starting a new compilation...")
+    })
+    compiler.hooks.compile.tap('LogWebpackPlugin', () => {
+      // compile（'编译器'对'开始编译'这个事件的监听）
+      console.log("The compiler is starting to compile...")
     })
   }
 }
